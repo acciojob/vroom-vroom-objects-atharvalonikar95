f7ab3d6c81f056f174f410c2b851ea5dc57cd1d6
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-20 01:22:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Complete the js code
 function Car(make, model) {
-	this._make=make
-	this._model=model
+	this.make=make
+	this.model=model
 
 }
 
 // Car.prototype=Object.create()
 Car.prototype.constructor=Car
 Car.prototype.getMakeModel=function(){
-		return this._make +" "+this._model
+		return this.make +" "+this.model
 	}
 
 function SportsCar(make, model, topSpeed) {
